6b5c5e1da41ba3e0f8a60a65d88c4aab77990186
summerFreeze6019 <summerFreeze6019@protonmail.com>
2024-07-09 17:10:49 +0300

    add new features

---
+++ scripts/showMoreOption.js
@@ -0,0 +1,63 @@
+function ShowMoreImages() {
+    var divider = document.getElementById("divider");
+    var moreImg = document.getElementById("moreImg");
+    var btnText = document.getElementById("imgBtn");
+
+    if (divider.style.display === "none") {
+        divider.style.display = "inline";
+        btnText.innerHTML = "Toon meer";
+        moreImg.style.display = "none";
+    } else {
+        divider.style.display = "none";
+        btnText.innerHTML = "Toon minder";
+        moreImg.style.display = "inline";
+    }
+}
+
+function ShowMoreImages2() {
+    var divider2 = document.getElementById("divider2");
+    var moreImg2 = document.getElementById("moreImg2");
+    var btnText2 = document.getElementById("imgBtn2");
+
+    if (divider2.style.display === "none") {
+        divider2.style.display = "inline";
+        btnText2.innerHTML = "Toon meer";
+        moreImg2.style.display = "none";
+    } else {
+        divider2.style.display = "none";
+        btnText2.innerHTML = "Toon minder";
+        moreImg2.style.display = "inline";
+    }
+}
+
+function ShowMoreImages3() {
+    var divider3 = document.getElementById("divider3");
+    var moreImg3 = document.getElementById("moreImg3");
+    var btnText3 = document.getElementById("imgBtn3");
+
+    if (divider3.style.display === "none") {
+        divider3.style.display = "inline";
+        btnText3.innerHTML = "Toon meer";
+        moreImg3.style.display = "none";
+    } else {
+        divider3.style.display = "none";
+        btnText3.innerHTML = "Toon minder";
+        moreImg3.style.display = "inline";
+    }
+}
+
+function ShowMoreImages4() {
+    var divider4 = document.getElementById("divider4");
+    var moreImg4 = document.getElementById("moreImg4");
+    var btnText4 = document.getElementById("imgBtn4");
+
+    if (divider4.style.display === "none") {
+        divider4.style.display = "inline";
+        btnText4.innerHTML = "Toon meer";
+        moreImg4.style.display = "none";
+    } else {
+        divider4.style.display = "none";
+        btnText4.innerHTML = "Toon minder";
+        moreImg4.style.display = "inline";
+    }
+}
